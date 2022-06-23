fb758c1a9c3f7cb6c003b70adf05b4f471f1786b
loczzs <le.baoloczzs@gmail.com>
2022-06-23 23:26:14 +0700

    first

---
+++ main.js
@@ -0,0 +1,9 @@
+function closEl(){
+    document.getElementById("sideEl").style.display = "none"
+    console.log('àafafafaf')
+}
+
+function opEl(){
+    document.getElementById("sideEl").style.display = "block"
+    console.log('àafafafaf')
+}
